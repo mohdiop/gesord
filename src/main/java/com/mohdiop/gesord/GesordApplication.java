f09f3f425f1d3398fdca512d21b17f3ff4a00e0b
Mohamed Diop <mohdiopcode@gmail.com>
2025-06-25 12:43:09 +0000

    Initialise le projet

---
+++ src/main/java/com/mohdiop/gesord/GesordApplication.java
@@ -0,0 +1,13 @@
+package com.mohdiop.gesord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohdiop/gesord/GesordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohdiop.gesord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
